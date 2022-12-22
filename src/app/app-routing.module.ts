f3e40880175a9c50ad572b3d216ae34a80221ab7
Soroushby <sorosh.bayanati@yahoo.com>
2022-12-22 16:25:47 -0700

    added ngrx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'gameOfThrones',
+    loadChildren: () =>
+      import('./game-of-thrones/game-of-thrones.module').then(
+        (m) => m.GameOfThronesModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
